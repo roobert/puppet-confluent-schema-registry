86d72a69a19fc19ee0818cac4d76bd4b1097ac33
Rob Wilson <roobert@gmail.com>
2015-12-11 16:43:37 +0000

    ensure service running

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,6 +1,6 @@
 class confluent_schema_registry::service {
   service { 'schema_registry':
-    ensure     => 'schema-registry',
+    ensure     => running,
     enable     => true,
     hasstatus  => true,
     hasrestart => true,
