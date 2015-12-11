210f974b6b5d18095391ad0d7688300de96a4932
Rob Wilson <roobert@gmail.com>
2015-12-11 16:46:40 +0000

    add service name

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,5 +1,6 @@
 class confluent_schema_registry::service {
   service { 'schema_registry':
+    name       => 'schema-registry',
     ensure     => running,
     enable     => true,
     hasstatus  => true,
