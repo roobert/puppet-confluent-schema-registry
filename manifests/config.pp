91d0ebca7673bfe4a60d0485aa9e1b1fa9e9155b
Rob Wilson <roobert@gmail.com>
2015-12-11 16:50:17 +0000

    syntax

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,6 +1,6 @@
 class confluent_schema_registry::config {
   file { '/etc/schema-registry/schema-registry.properties':
-    content => template('confluent_schema_registry/schema-registry.properties.erb')
+    content => template('confluent_schema_registry/schema-registry.properties.erb'),
     owner   => 'schema-registry',
     group   => 'schema-registry',
     mode    => '0644',
