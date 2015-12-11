680ccf03ab8a458054ac055b2c78b9cc4301594c
Rob Wilson <roobert@gmail.com>
2015-12-11 16:30:21 +0000

    syntax

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,4 +1,4 @@
-class confluent_schema_registry::params (
+class confluent_schema_registry::params {
   $version                   = '1.0.1-2'
   $kafkastore_connection_url = 'localhost:2181'
   $kafkastore_topic          = '_schemas'
