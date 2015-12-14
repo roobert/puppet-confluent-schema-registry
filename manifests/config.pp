37cf4f9a4813a5d0ad7f0e9b555eff231c54ae2c
Rob Wilson <roobert@gmail.com>
2015-12-14 11:07:42 +0000

    change locations of defaults

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,4 +1,9 @@
-class confluent_schema_registry::config {
+class confluent_schema_registry::config (
+  $kafkastore_connection_url = 'localhost:2181',
+  $kafkastore_topic          = '_schemas',
+  $debug                     = 'false',
+  $port                      = '8081',
+) {
   file { '/etc/schema-registry/schema-registry.properties':
     content => template('confluent_schema_registry/schema-registry.properties.erb'),
     owner   => 'schema-registry',
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,5 @@
 class confluent_schema_registry (
   $version                   = '1.0.1-2',
-  $kafkastore_connection_url = 'localhost:2181',
-  $kafkastore_topic          = '_schemas',
-  $debug                     = 'false',
-  $port                      = '8081',
 ) {
   class { 'confluent_schema_registry::install': } ->
   class { 'confluent_schema_registry::config': } ->
