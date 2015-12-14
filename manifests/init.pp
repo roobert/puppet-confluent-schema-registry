e9fc8834ab8216df4283f8432081dacec1cb5ddf
Rob Wilson <roobert@gmail.com>
2015-12-14 11:00:51 +0000

    test without params.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,10 @@
 class confluent_schema_registry (
-  $kafkastore_connection_url = $::confluent_schema_registry::params::kafkastore_connection_url,
-  $kafkastore_topic          = $::confluent_schema_registry::params::kafkastore_topic,
-  $debug                     = $::confluent_schema_registry::params::debug,
-  $port                      = $::confluent_schema_registry::params::port,
-  $version                   = $::confluent_schema_registry::params::version,
-) inherits ::confluent_schema_registry::params {
+  $version                   = '1.0.1-2',
+  $kafkastore_connection_url = 'localhost:2181',
+  $kafkastore_topic          = '_schemas',
+  $debug                     = 'false',
+  $port                      = '8081',
+) {
   class { 'confluent_schema_registry::install': } ->
   class { 'confluent_schema_registry::config': } ->
   class { 'confluent_schema_registry::service': } ->
