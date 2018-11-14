2ff490eb9ba98be600170dc700b82450a3060001
Dennis Dryden <dennis@brandwatch.com>
2018-11-14 11:45:19 +0000

    Add settings to allow schema registrys to share a ZK.

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,6 +2,10 @@ class confluent_schema_registry::config (
   $port                                = '8081',
   $debug                               = 'false',
   $kafkastore_connection_url           = 'localhost:2181',
+  $zk_namespace                        = undef,
+  $group_id                            = undef,
+  $master_eligibility                  = 'true',
+  $unclean_leader_election_enable      = 'false',
   $kafkastore_topic                    = '_schemas',
   $kafkastore_topic_replication_factor = '3',
   $kafkastore_commit_interval_ms       = '-1',
@@ -16,7 +20,7 @@ class confluent_schema_registry::config (
   $request_logger_name                 = 'io.confluent.rest-utils.requests',
   $shutdown_graceful_ms                = '1000',
 ) {
-  file { 
+  file {
     '/etc/schema-registry/schema-registry.properties':
       content => template('confluent_schema_registry/schema-registry.properties.erb'),
       owner   => 'schema-registry',
